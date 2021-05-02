208b22b54f1fcc43cfa90e29d813184cf779c8aa
Murilo Arantes <murilo.arantess@gmail.com>
2021-05-02 01:55:43 -0300

    Implementação final

--- logs.go
+++ logs.go
@@ -1,159 +1,150 @@
 package logs
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
+	"io"
 	"log"
-	"math/rand"
 	"os"
-	"sync"
-	"time"
+	"strings"
+)
+
+const (
+	logInfoPrefix  = "INFO"
+	logErrorPrefix = "ERROR"
+	logFatalPrefix = "FATAL"
 )
 
 var (
-	logr            *logger
-	shutdownChannel chan struct{}
-	shutdownMode    = true
-	bufPool         = sync.Pool{
-		New: func() interface{} {
-			return new(bytes.Buffer)
-		},
-	}
-	waitIncreasedChannel chan struct{}
+	globalLogger *Logger
 )
 
 func init() {
-	waitIncreasedChannel = make(chan struct{})
+	SetGlobalLogger(NewLogger())
+	log.SetFlags(log.LstdFlags)
 }
 
-// Shutdown wait as logs complete
-func Shutdown() {
-	shutdownMode = true
-	for {
-		if len(logr.messagesChannel) == 0 {
-			break
-		}
-	}
-	close(shutdownChannel)
+// Init to log using the Writer
+func Init(writer io.Writer) {
+	log.SetOutput(writer)
 }
 
-// LogMessageQueueSize returns the size of the logs message queue
-func LogMessageQueueSize() int {
-	if logr == nil {
-		return 0
-	}
-	return cap(logr.messagesChannel)
-}
-
-// NumberOfMessagesQueued returns the amount of the logs message in the queue
-func NumberOfMessagesQueued() int {
-	if logr == nil {
-		return 0
-	}
-	return len(logr.messagesChannel)
-}
-
-// SetupPerDay set the standard log to file and rotate it per day
-func SetupPerDay(logPath string, logMessagesQueueSize int) error {
-	nextDateLogFileFunc := func(now time.Time) time.Time {
-		return time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, time.Local)
-	}
-	fileDateFormatFunc := func() string {
-		return "2006-01-02"
-	}
-	return setup(nextDateLogFileFunc, fileDateFormatFunc, logPath, logMessagesQueueSize)
-}
-
-type logger struct {
-	messagesChannel chan *bytes.Buffer
-	logFile         *os.File
-	logPath         string
-	mux             sync.Mutex
-}
-
-func (l *logger) Write(p []byte) (int, error) {
-	buf := bufPool.Get().(*bytes.Buffer)
-	if n, err := buf.Write(p); err != nil {
-		buf.Reset()
-		bufPool.Put(buf)
-		return n, err
-	}
-	l.mux.Lock()
-	defer l.mux.Unlock()
-	select {
-	case l.messagesChannel <- buf:
-	default:
-		messagesChannelIncreased := make(chan *bytes.Buffer, cap(l.messagesChannel)*2)
-		for {
-			if len(logr.messagesChannel) == 0 {
-				break
-			}
-		}
-		waitIncreasedChannel <- struct{}{}
-		l.messagesChannel = messagesChannelIncreased
-		waitIncreasedChannel <- struct{}{}
-		l.messagesChannel <- buf
-	}
-	return len(p), nil
-}
-
-func setup(nextDateLogFileFunc func(now time.Time) time.Time, fileDateFormatFunc func() string, logPath string, logMessagesQueueSize int) error {
-	shutdownMode = false
-	shutdownChannel = make(chan struct{})
-	filePathTest := fmt.Sprintf("%s%d", logPath, rand.Int())
-	_, err := os.Create(filePathTest)
+// InitLogFile to log using file
+func InitLogFile(filename string) error {
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
 	if err != nil {
-		return errors.New("It was not possible to log in " + logPath)
+		return err
 	}
-	os.Remove(filePathTest)
-	logr = &logger{
-		messagesChannel: make(chan *bytes.Buffer, logMessagesQueueSize),
-		logPath:         logPath,
-	}
-	openLogFile(nextDateLogFileFunc, fileDateFormatFunc)
-	startMessagesLogger()
-	log.SetOutput(logr)
+	Init(f)
 	return nil
 }
 
-func openLogFile(nextDateLogFileFunc func(time.Time) time.Time, fileDateFormatFunc func() string) {
-	if shutdownMode {
-		return
-	}
-	now := time.Now()
-	logPathWithDate := fmt.Sprintf("%s-%s.log", logr.logPath, now.Format(fileDateFormatFunc()))
-	if _, err := os.Stat(logPathWithDate); os.IsNotExist(err) && logr.logFile != nil {
-		oldFile := logr.logFile
-		defer oldFile.Close()
-	}
-	file, err := os.OpenFile(logPathWithDate, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err == nil {
-		logr.logFile = file
-		time.AfterFunc(nextDateLogFileFunc(now).Sub(now), func() {
-			log.Println("Rotating log file...")
-			openLogFile(nextDateLogFileFunc, fileDateFormatFunc)
-		})
-	} else {
-		panic(err)
-	}
+// NewLogger gets a new Logger
+func NewLogger(fixedValues ...FieldValue) *Logger {
+	return &Logger{fixedValues[:]}
 }
 
-func startMessagesLogger() {
-	go func() {
-		for {
-			select {
-			case b := <-logr.messagesChannel:
-				if _, err := b.WriteTo(logr.logFile); err != nil {
-					panic(err)
-				}
-				b.Reset()
-				bufPool.Put(b)
-			case <-waitIncreasedChannel:
-				<-waitIncreasedChannel
-			case <-shutdownChannel:
-				return
-			}
-		}
-	}()
+// NewChildLogger gets a new Logger based in global Logger
+func NewChildLogger(fixedValues ...FieldValue) *Logger {
+	globalFixedValues := globalLogger.fixedValues[:]
+	globalFixedValues = append(globalFixedValues, fixedValues...)
+	return &Logger{globalFixedValues}
+}
+
+// SetGlobalLogger set global logger
+func SetGlobalLogger(l *Logger) {
+	globalLogger = l
+}
+
+// FixedValue gets a FieldValue
+func FixedValue(key string, val interface{}) FieldValue {
+	return FieldValue{Key: key, Val: val}
+}
+
+// Fatal logs using the globalLogger
+func Fatal(message interface{}) {
+	globalLogger.Fatal(message)
+}
+
+// Info logs using the globalLogger
+func Info(message interface{}) {
+	globalLogger.Info(message)
+}
+
+// Error logs using the globalLogger
+func Error(message interface{}) {
+	globalLogger.Error(message)
+}
+
+// Fatalf logs using the globalLogger
+func Fatalf(message string, v ...interface{}) {
+	globalLogger.Fatalf(message, v...)
+}
+
+// Infof logs using the globalLogger
+func Infof(message string, v ...interface{}) {
+	globalLogger.Infof(message, v...)
+}
+
+// Errorf logs using the globalLogger
+func Errorf(message string, v ...interface{}) {
+	globalLogger.Errorf(message, v...)
+}
+
+type Logger struct {
+	fixedValues []FieldValue
+}
+
+func (l *Logger) Fatalf(message string, v ...interface{}) {
+	log.Fatal(l.format(logFatalPrefix, message, v...))
+}
+
+func (l *Logger) Infof(message string, v ...interface{}) {
+	log.Println(l.format(logInfoPrefix, message, v...))
+}
+
+func (l *Logger) Errorf(message string, v ...interface{}) {
+	log.Println(l.format(logErrorPrefix, message, v...))
+}
+
+func (l *Logger) Fatal(message interface{}) {
+	log.Fatal(l.print(logFatalPrefix, message))
+}
+
+func (l *Logger) Info(message interface{}) {
+	log.Println(l.print(logInfoPrefix, message))
+}
+
+func (l *Logger) Error(message interface{}) {
+	log.Println(l.print(logErrorPrefix, message))
+}
+
+func (l *Logger) print(level string, message interface{}) string {
+	builder := l.logTemplate(level)
+	builder.WriteString(fmt.Sprint(message))
+	return builder.String()
+}
+
+func (l *Logger) format(level, message string, v ...interface{}) string {
+	builder := l.logTemplate(level)
+	builder.WriteString(fmt.Sprintf(message, v...))
+	return builder.String()
+}
+
+func (l *Logger) logTemplate(level string) *strings.Builder {
+	builder := strings.Builder{}
+	builder.WriteString(level)
+	for _, fv := range l.fixedValues {
+		builder.WriteString(" [")
+		builder.WriteString(fv.Key)
+		builder.WriteString(": ")
+		builder.WriteString(fmt.Sprint(fv.Val))
+		builder.WriteString("]")
+	}
+	builder.WriteString(" * ")
+	return &builder
+}
+
+type FieldValue struct {
+	Key string
+	Val interface{}
 }
--- logs_test.go
+++ logs_test.go
@@ -2,144 +2,101 @@ package logs
 
 import (
 	"io/ioutil"
-	"log"
 	"os"
-	"strconv"
 	"strings"
-	"sync"
 	"testing"
-	"time"
 )
 
-func TestIfSetupShutdownAndRotateLogFileWorks(t *testing.T) {
-	dir := createTempDir(t)
-	defer cleanup(dir, t)
-	t.Log(dir)
-	logFile := dir + string(os.PathSeparator) + "testfile"
-	rotateAfterOneSecond := func(now time.Time) time.Time {
-		return time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second()+1, 0, time.Local)
-	}
-	rotateSecondLogFile := func() string {
-		return "2006-01-02-15-04-5"
-	}
-	setup(rotateAfterOneSecond, rotateSecondLogFile, logFile, 5)
-	txts := []string{"text-1", "text-2", "text-3", "text-4"}
-	for i, txt := range txts {
-		log.Println(txt)
-		if i < len(txts)-1 {
-			time.Sleep(time.Second)
-		}
-	}
-	Shutdown()
-	files, err := ioutil.ReadDir(dir)
+var logWriter = logWriterTest{}
+
+func TestShouldLogSimpleMessage(t *testing.T) {
+	setup()
+	Info("teste")
+	logWriter.assertLogMessage(t, "INFO * teste\n")
+	Error("teste")
+	logWriter.assertLogMessage(t, "ERROR * teste\n")
+}
+
+func TestShouldLogFormattedMessage(t *testing.T) {
+	setup()
+	Infof("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "INFO * teste txt 10\n")
+	Errorf("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "ERROR * teste txt 10\n")
+}
+
+func TestShouldLogSimpleMessageWithFixedFields(t *testing.T) {
+	setup()
+	l := NewLogger(FixedValue("reqid", "1"), FixedValue("idtperson", "2"))
+	l.Info("teste")
+	logWriter.assertLogMessage(t, "INFO [reqid: 1] [idtperson: 2] * teste\n")
+	l.Error("teste")
+	logWriter.assertLogMessage(t, "ERROR [reqid: 1] [idtperson: 2] * teste\n")
+}
+
+func TestShouldLogFormattedMessageWithFixedFields(t *testing.T) {
+	setup()
+	l := NewLogger(FixedValue("reqid", "1"), FixedValue("idtperson", "2"))
+	l.Infof("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "INFO [reqid: 1] [idtperson: 2] * teste txt 10\n")
+	l.Errorf("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "ERROR [reqid: 1] [idtperson: 2] * teste txt 10\n")
+}
+
+func TestShouldLogBasedInGlobalLoggerWithFixedFields(t *testing.T) {
+	setup()
+	SetGlobalLogger(NewLogger(FixedValue("reqid", "1")))
+	Infof("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "INFO [reqid: 1] * teste txt 10\n")
+	Errorf("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "ERROR [reqid: 1] * teste txt 10\n")
+
+	l := NewChildLogger(FixedValue("idtperson", "2"))
+	l.Infof("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "INFO [reqid: 1] [idtperson: 2] * teste txt 10\n")
+	l.Errorf("teste %s %d", "txt", 10)
+	logWriter.assertLogMessage(t, "ERROR [reqid: 1] [idtperson: 2] * teste txt 10\n")
+}
+
+func TestShouldLogInFile(t *testing.T) {
+	setup()
+	f, err := ioutil.TempFile("", "test-go-log.log")
 	if err != nil {
 		t.Fatal(err)
 	}
-	if len(files) != len(txts) {
-		t.Fatalf("Folder should %d files, but it has %d", len(txts), len(files))
+	defer os.Remove(f.Name())
+	err = InitLogFile(f.Name())
+	if err != nil {
+		t.Fatal(err)
 	}
-	for i, txt := range txts {
-		if content, err := fileContent(dir, files[i]); err != nil || !strings.Contains(content, txt) {
-			t.Fatalf("Folder expected %s, but received %s. Error: %v", txt, content, err)
-		}
+	Info("teste")
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Fatal(err)
+	}
+	const expected = "INFO * teste\n"
+	if !strings.HasSuffix(string(b), expected) {
+		t.Fatalf("Expected '%s', but '%s' was logged\n", expected, string(b))
 	}
 }
 
-func TestIfTheLogMessageQueueWasIncreasedWhenThereWereManyLogsOperations(t *testing.T) {
-	dir := createTempDir(t)
-	defer cleanup(dir, t)
-	t.Log(dir)
-	logFile := dir + string(os.PathSeparator) + "testfile"
-	err := SetupPerDay(logFile, 3)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var wg sync.WaitGroup
-	const expectedLines = 2000
-	for i := 1; i <= expectedLines; i++ {
-		go func(ind int) {
-			wg.Add(1)
-			log.Println(strings.Repeat("a", ind))
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
-	Shutdown()
-	if LogMessageQueueSize() <= 3 {
-		t.Fatal("Log message queue size did not increased")
-	}
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(files) != 1 {
-		t.Fatalf("There should be one log file, but there are %d\n", len(files))
-	}
-	txt, err := fileContent(dir, files[0])
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(strings.Split(txt, "\n")) != expectedLines+1 {
-		t.Fatal("logs lost", LogMessageQueueSize(), len(strings.Split(txt, "\n")))
+func setup() {
+	SetGlobalLogger(NewLogger())
+	Init(&logWriter)
+}
+
+type logWriterTest struct {
+	lastLog string
+}
+
+func (w *logWriterTest) Write(p []byte) (n int, err error) {
+	w.lastLog = string(p)
+	return len(p), nil
+}
+
+func (w *logWriterTest) assertLogMessage(t *testing.T, m string) {
+	if !strings.HasSuffix(w.lastLog, m) {
+		t.Fatalf("Expected '%s', but '%s' was logged\n", w.lastLog, m)
 	}
 }
 
-func TestIfTheLogMessageWereLoggedInRightOrder(t *testing.T) {
-	dir := createTempDir(t)
-	defer cleanup(dir, t)
-	t.Log(dir)
-	logFile := dir + string(os.PathSeparator) + "testfile"
-	err := SetupPerDay(logFile, 3)
-	if err != nil {
-		t.Fatal(err)
-	}
-	var wg sync.WaitGroup
-	const expectedLines = 2000
-	for i := 1; i <= expectedLines; i++ {
-		log.Println(i)
-	}
-	wg.Wait()
-	Shutdown()
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(files) != 1 {
-		t.Fatalf("There should be one log file, but there are %d\n", len(files))
-	}
-	txt, err := fileContent(dir, files[0])
-	if err != nil {
-		t.Fatal(err)
-	}
-	linesOfTxt := strings.Split(txt, "\n")
-	if len(linesOfTxt) != expectedLines+1 {
-		t.Fatal("logs lost", LogMessageQueueSize(), len(strings.Split(txt, "\n")))
-	}
-	for i := 0; i < expectedLines; i++ {
-		if !strings.HasSuffix(linesOfTxt[i], strconv.Itoa(i+1)) {
-			t.Fatalf("Invalid log order in line %d. Expected: %d - Received: %s", i+1, i+1, linesOfTxt[i])
-		}
-	}
-}
-
-func fileContent(dir string, fileInfo os.FileInfo) (string, error) {
-	b, err := ioutil.ReadFile(dir + string(os.PathSeparator) + fileInfo.Name())
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
-}
-
-func createTempDir(t *testing.T) string {
-	dir, err := ioutil.TempDir("", t.Name())
-	if err != nil {
-		t.Fatal(err)
-	}
-	return dir
-}
-
-func cleanup(dir string, t *testing.T) {
-	if !t.Failed() {
-		os.RemoveAll(dir)
-	}
-}
